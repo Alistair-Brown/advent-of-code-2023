64342a987938502d663a8d67a3da81c8703bd940
Alistair Brown <ali.b_houghton@hotmail.com>
2023-12-01 21:28:35 +0000

    Part one tidy up and 2x speed up

---
+++ advent_of_code_2023/Source/Day 01/CalibrationValue.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC.DayOne
+{
+    internal class CalibrationValue
+    {
+        public readonly int leftHandValue;
+        public readonly int rightHandValue;
+
+        public CalibrationValue(string stringRepresentation, int value) 
+        {
+            this.stringRepresentation = stringRepresentation;
+            this.stringLength = stringRepresentation.Length;
+            this.leftHandValue = value * 10;
+            this.rightHandValue = value;
+        }
+
+        private readonly string stringRepresentation;
+        private readonly int stringLength;
+
+        private int charactersMatchedFromLeft = 0;
+        private int charactersMatchedFromRight = 0;
+
+        public void Reset()
+        {
+            charactersMatchedFromLeft = 0;
+            charactersMatchedFromRight = 0;
+        }
+
+        public bool MatchCompletedFromLeft(char nextCharacter)
+        {
+            if (nextCharacter == stringRepresentation[charactersMatchedFromLeft])
+            {
+                ++charactersMatchedFromLeft;
+                if (charactersMatchedFromLeft == stringLength)
+                {
+                    return true;
+                }
+                return false;
+            }
+            else if (nextCharacter == stringRepresentation[0])
+            {
+                charactersMatchedFromLeft = 1;
+                return false;
+            }
+            else
+            {
+                charactersMatchedFromLeft = 0;
+                return false;
+            }
+        }
+        public bool MatchCompletedFromRight(char nextCharacter)
+        {
+            if (nextCharacter == stringRepresentation[stringLength - 1 - charactersMatchedFromRight])
+            {
+                ++charactersMatchedFromRight;
+                if (charactersMatchedFromRight == stringLength)
+                {
+                    return true;
+                }
+                return false;
+            }
+            else if (nextCharacter == stringRepresentation[stringLength - 1])
+            {
+                charactersMatchedFromRight = 1;
+                return false;
+            }
+            else
+            {
+                charactersMatchedFromRight = 0;
+                return false;
+            }
+        }
+    }
+}
--- advent_of_code_2023/Source/Day 01/DayOneSolver.cs
+++ advent_of_code_2023/Source/Day 01/DayOneSolver.cs
@@ -5,159 +5,153 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
+using AOC.DayOne;
+
 namespace AOC
 {
     internal class DayOneSolver : IPuzzleSolver
     {
         public PuzzleSolution SolvePuzzle(string[] puzzleInputLines)
         {
-            ulong partOneSum = 0;
-            ulong partTwoSum = 0;
+            int partOneSum = 0;
+            int partTwoSum = 0;
 
-            foreach(string line in puzzleInputLines)
-            {
-                string digitOne = "0";
-                string digitTwo = "0";
-                foreach (int ii in Enumerable.Range(0, line.Length))
-                {
-                    if (line[ii] >= '0' && line[ii] <= '9')
-                    {
-                        digitOne = Char.ToString(line[ii]);
-                        break;
-                    }
-                }
+            CalibrationValue[] partOneCalibrationValues = new CalibrationValue[]{
+                new CalibrationValue("0", 0),
+                new CalibrationValue("1", 1),
+                new CalibrationValue("2", 2),
+                new CalibrationValue("3", 3),
+                new CalibrationValue("4", 4),
+                new CalibrationValue("5", 5),
+                new CalibrationValue("6", 6),
+                new CalibrationValue("7", 7),
+                new CalibrationValue("8", 8),
+                new CalibrationValue("9", 9)
+            };
 
-                foreach (int ii in Enumerable.Range(0, line.Length).Reverse())
-                {
-                    if (line[ii] >= '0' && line[ii] <= '9')
-                    {
-                        digitTwo = Char.ToString(line[ii]);
-                        break;
-                    }
-                }
-
-                partOneSum += Convert.ToUInt64(digitOne + digitTwo);
-            }
+            CalibrationValue[] partTwoAdditionalCalibrationValues = new CalibrationValue[]{
+                new CalibrationValue("one", 1),
+                new CalibrationValue("two", 2),
+                new CalibrationValue("three", 3),
+                new CalibrationValue("four", 4),
+                new CalibrationValue("five", 5),
+                new CalibrationValue("six", 6),
+                new CalibrationValue("seven", 7),
+                new CalibrationValue("eight", 8),
+                new CalibrationValue("nine", 9)
+            };
 
             foreach (string line in puzzleInputLines)
             {
-                string digitOne = "0";
-                string digitTwo = "0";
-                int digitPos = 0;
+                bool partOneFound = false;
+                bool partTwoFound = false;
+                int partOneValue = 0;
+                int partTwoValue = 0;
 
-                foreach (int ii in Enumerable.Range(0, line.Length))
+                for (int ii = 0; ii < line.Length; ++ii)
                 {
-                    if (line[ii] >= '0' && line[ii] <= '9')
+                    char charFromLeft = line[ii];
+
+                    // Start by checking for a match from the part one values, since this will also count as
+                    // a match for part two if we haven't already found one.
+                    foreach(CalibrationValue calibrationValue in partOneCalibrationValues)
                     {
-                        digitOne = Char.ToString(line[ii]);
-                        digitPos = ii;
+                        if (calibrationValue.MatchCompletedFromLeft(charFromLeft))
+                        {
+                            partOneFound = true;
+                            partOneValue += calibrationValue.leftHandValue;
+
+                            if (!partTwoFound)
+                            {
+                                partTwoFound = true;
+                                partTwoValue += calibrationValue.leftHandValue;
+                            }
+                            break;
+                        }
+                    }
+
+                    if (!partTwoFound)
+                    {
+                        foreach (CalibrationValue calibrationValue in partTwoAdditionalCalibrationValues)
+                        {
+                            if (calibrationValue.MatchCompletedFromLeft(charFromLeft))
+                            {
+                                partTwoFound = true;
+                                partTwoValue += calibrationValue.leftHandValue;
+                                break;
+                            }
+                        }
+                    }
+                    
+                    if (partOneFound && partTwoFound)
+                    {
+                        foreach (CalibrationValue calibrationValue in partOneCalibrationValues)
+                        {
+                            calibrationValue.Reset();
+                        }
+                        foreach (CalibrationValue calibrationValue in partTwoAdditionalCalibrationValues)
+                        {
+                            calibrationValue.Reset();
+                        }
+
                         break;
                     }
                 }
 
-                if (line.IndexOf("one") <= digitPos && line.IndexOf("one") >= 0)
-                {
-                    digitOne = "1";
-                    digitPos = line.IndexOf("one");
-                }
-                if (line.IndexOf("two") <= digitPos && line.IndexOf("two") >= 0)
-                {
-                    digitOne = "2";
-                    digitPos = line.IndexOf("two");
-                }
-                if (line.IndexOf("three") <= digitPos && line.IndexOf("three") >= 0)
-                {
-                    digitOne = "3";
-                    digitPos = line.IndexOf("three");
-                }
-                if (line.IndexOf("four") <= digitPos && line.IndexOf("four") >= 0)
-                {
-                    digitOne = "4";
-                    digitPos = line.IndexOf("four");
-                }
-                if (line.IndexOf("five") <= digitPos && line.IndexOf("five") >= 0)
-                {
-                    digitOne = "5";
-                    digitPos = line.IndexOf("five");
-                }
-                if (line.IndexOf("six") <= digitPos && line.IndexOf("six") >= 0)
-                {
-                    digitOne = "6";
-                    digitPos = line.IndexOf("six");
-                }
-                if (line.IndexOf("seven") <= digitPos && line.IndexOf("seven") >= 0)
-                {
-                    digitOne = "7";
-                    digitPos = line.IndexOf("seven");
-                }
-                if (line.IndexOf("eight") <= digitPos && line.IndexOf("eight") >= 0)
-                {
-                    digitOne = "8";
-                    digitPos = line.IndexOf("eight");
-                }
-                if (line.IndexOf("nine") <= digitPos && line.IndexOf("nine") >= 0)
-                {
-                    digitOne = "9";
-                    digitPos = line.IndexOf("nine");
-                }
+                partOneFound = false;
+                partTwoFound = false;
 
-                foreach (int ii in Enumerable.Range(0, line.Length).Reverse())
+                for (int ii = line.Length - 1; ii >= 0; --ii)
                 {
-                    if (line[ii] >= '0' && line[ii] <= '9')
+                    char charFromRight = line[ii];
+
+                    foreach (CalibrationValue calibrationValue in partOneCalibrationValues)
                     {
-                        digitTwo = Char.ToString(line[ii]);
-                        digitPos = ii;
+                        if (calibrationValue.MatchCompletedFromRight(charFromRight))
+                        {
+                            partOneFound = true;
+                            partOneValue += calibrationValue.rightHandValue;
+
+                            if (!partTwoFound)
+                            {
+                                partTwoFound = true;
+                                partTwoValue += calibrationValue.rightHandValue;
+                            }
+
+                            break;
+                        }
+                    }
+
+                    if (!partTwoFound)
+                    {
+                        foreach (CalibrationValue calibrationValue in partTwoAdditionalCalibrationValues)
+                        {
+                            if (calibrationValue.MatchCompletedFromRight(charFromRight))
+                            {
+                                partTwoFound = true;
+                                partTwoValue += calibrationValue.rightHandValue;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (partOneFound && partTwoFound)
+                    {
+                        foreach (CalibrationValue calibrationValue in partOneCalibrationValues)
+                        {
+                            calibrationValue.Reset();
+                        }
+                        foreach (CalibrationValue calibrationValue in partTwoAdditionalCalibrationValues)
+                        {
+                            calibrationValue.Reset();
+                        }
+
                         break;
                     }
                 }
 
-                if (line.LastIndexOf("one") > digitPos)
-                {
-                    digitTwo = "1";
-                    digitPos = line.LastIndexOf("one");
-                }
-                if (line.LastIndexOf("two") > digitPos)
-                {
-                    digitTwo = "2";
-                    digitPos = line.LastIndexOf("two");
-                }
-                if (line.LastIndexOf("three") > digitPos)
-                {
-                    digitTwo = "3";
-                    digitPos = line.LastIndexOf("three");
-                }
-                if (line.LastIndexOf("four") > digitPos)
-                {
-                    digitTwo = "4";
-                    digitPos = line.LastIndexOf("four");
-                }
-                if (line.LastIndexOf("five") > digitPos)
-                {
-                    digitTwo = "5";
-                    digitPos = line.LastIndexOf("five");
-                }
-                if (line.LastIndexOf("six") > digitPos)
-                {
-                    digitTwo = "6";
-                    digitPos = line.LastIndexOf("six");
-                }
-                if (line.LastIndexOf("seven") > digitPos)
-                {
-                    digitTwo = "7";
-                    digitPos = line.LastIndexOf("seven");
-                }
-                if (line.LastIndexOf("eight") > digitPos)
-                {
-                    digitTwo = "8";
-                    digitPos = line.LastIndexOf("eight");
-                }
-                if (line.LastIndexOf("nine") > digitPos)
-                {
-                    digitTwo = "9";
-                    digitPos = line.LastIndexOf("nine");
-                }
-
-                partTwoSum += Convert.ToUInt64(digitOne + digitTwo);
+                partOneSum += partOneValue;
+                partTwoSum += partTwoValue;
             }
 
             return new PuzzleSolution(Convert.ToString(partOneSum), Convert.ToString(partTwoSum));
