fc9706bb578e157cd5cb91ef6b65e228cf8aa5d1
Alistair Brown <ali.b_houghton@hotmail.com>
2023-12-06 08:50:26 +0000

    Solve day 6 as a quadratic to get the execution time under 1ms

--- advent_of_code_2023/Source/Day06/DaySixSolver.cs
+++ advent_of_code_2023/Source/Day06/DaySixSolver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,27 +33,43 @@ namespace AOC
             return new PuzzleSolution(partOneWaysToWin.ToString(), partTwoWaysToWin.ToString());
         }
 
-        // There's definitely a maths-y way of working this out with O(n), should
-        // find that.
-        private int WaysToWinRace(ulong time, ulong distance)
+        // To solve with O(n) complexity, we can solve a quadratic equation.
+        // The optimal amount of time to hold the button down for is half the
+        // total time. Hold down a ms longer you gain 1ms of speed to use in
+        // ((time / 2) - 1) time, but lose 1ms of time to use the (time / 2) speed
+        // you've already gain, which is a net loss. Equivalent applies for holding
+        // 1ms shorter. The optimum time held gives you (time/2) speed to use in
+        // (time/2) time, giving you (time/2)^2 distance.
+        // As you move away from this optimum by holding down for longer, the formula
+        // for distance as speed * time becomes:
+        // ((time / 2) + extraTimeHeld) * ((time / 2) - extraTimeHeld) = distance
+        // We need to rearrange this formula and find all integer values of total time
+        // held that exceed the required distance.
+        private int WaysToWinRace(ulong time, ulong previousWinningDistance)
         {
-            int waysToWin = 0;
-            for (ulong timeHeld = 1; timeHeld < time; ++timeHeld)
-            {
-                if (IsAWayToWin(time, timeHeld, distance))
-                {
-                    ++waysToWin;
-                }
-            }
+            int waysToWin;
+            ulong requiredDistance = previousWinningDistance + 1;
+
+            // Rearranged from formula in header comment, this finds how many extra ms we
+            // can hold down the button for and still reach the required distance.
+            double extraMsHeld = (Math.Sqrt((((double)time / 2) * (double)time / 2) - requiredDistance));
+
+            // The above didn't account for the fact that only integer hold times are valid.
+            // If our total time was even, then the optimum hold (half of that total) is an
+            // integer number of seconds. So we just need to floor the extraMsHeld to an integer
+            // double it to account the possibility of holding it for *less* than the optimum
+            // and then add 1 to make sure our total includes that central optimum value.
+            if (time % 2 == 0) { waysToWin = (int)Math.Floor(extraMsHeld) * 2 + 1; }
+            // Odd cases have their optimum position halfways between two values.
+            // So first valid step is 0.5ms from optimum, next at 1.5ms etc. To get the number
+            // of valid integer hold times, we can therefore subtract 0.5ms from our hold time, to
+            // instead get the extra hold time from the first of the two optimum integer values
+            // in the middle of the range, then floor and double this to get the integer values
+            // on either side of these midpoints, then +2 to add those optimum two integer midpoints
+            // back into our total.
+            else { waysToWin = (int)Math.Floor((extraMsHeld - 0.5f)) * 2 + 2; }
 
             return waysToWin;
-        }
-
-        private bool IsAWayToWin(ulong totalTime, ulong timeHeld, ulong distanceNeeded)
-        {
-            ulong speed = timeHeld;
-            ulong distanceTravelled = speed * (totalTime - timeHeld);
-            return distanceTravelled > distanceNeeded;
         }
     }
 }
