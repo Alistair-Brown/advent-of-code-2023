303fb526bc2aecec2c5ab6af6b9eb94f1f52d521
Alistair Brown <ali.b_houghton@hotmail.com>
2023-12-22 12:49:58 +0000

    Write the templates for the remaining 3 days

--- advent_of_code_2023/Source/Common/PuzzleSolvers.cs
+++ advent_of_code_2023/Source/Common/PuzzleSolvers.cs
@@ -16,7 +16,7 @@ namespace AOC
         public const string puzzleInputFileStem = "puzzle_input_";
         public const string puzzleInputExtension = ".txt";
 
-        public const int numberOfSolvers = 22;
+        public const int numberOfSolvers = 25;
         static public IPuzzleSolver[] puzzleSolvers = new IPuzzleSolver[numberOfSolvers] {
             new DayOneSolver(),
             new DayTwoSolver(),
@@ -39,6 +39,9 @@ namespace AOC
             new DayNineteenSolver(),
             new DayTwentySolver(),
             new DayTwentyOneSolver(),
-            new DayTwentyTwoSolver()};
+            new DayTwentyTwoSolver(),
+            new DayTwentyThreeSolver(),
+            new DayTwentyFourSolver(),
+            new DayTwentyFiveSolver()};
     }
 }
---
+++ advent_of_code_2023/Source/Day24/DayTwentyFourSolver.cs
@@ -0,0 +1,17 @@
+﻿using AOC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC
+{
+    internal class DayTwentyFourSolver : IPuzzleSolver
+    {
+        public PuzzleSolution SolvePuzzle(string[] puzzleInputLines)
+        {
+            return new PuzzleSolution("Hello", "World");
+        }
+    }
+}
